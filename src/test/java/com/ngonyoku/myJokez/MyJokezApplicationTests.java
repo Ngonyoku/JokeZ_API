e230062c333d7d696dae824cfa0f6c25e3dab216
Ngonyoku <ngonyoku001@gmail.com>
2021-02-20 17:33:49 +0300

    initial commit

---
+++ src/test/java/com/ngonyoku/myJokez/MyJokezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngonyoku.myJokez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJokezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
